9dca27002d89968b107cfd2e6a9f3304111cbe2e
Ryan Paul <seg@phault.net>
2014-09-26 22:38:46 -0700

    Tweaked configuration to run in Dokku

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 module.exports = {
   database: {
     db: "quake",
-    host: "localhost",
-    port: 28015
+    host: process.env.RDB_HOST || "localhost",
+    port: process.env.RDB_PORT || 28015
   },
 
   port: 8090
